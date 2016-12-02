b61ea97d7db3ff6d2b4a0f2604900fcab80dc780
jackyyu1209 <jackyyu1209@gmail.com>
2016-12-01 17:10:02 -0800

    Add files via upload
    commit 2

--- StringProblem.java
+++ StringProblem.java
@@ -3,25 +3,47 @@ Created on Dec 1, 2016
 
 @author: Jacky Yu
 **/
+package AppleTest;
 
-import java.util.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
 public class StringProblem {
-	
-	//Remove Char method
-	public static String reChar(String str, char re){
-        StringBuilder sb = new StringBuilder();
-        char[] charArray = str.toCharArray();
-        for(int i=0; i<charArray.length; i++){
-            if (charArray[i] == re){} //do nothing
-            else sb.append(charArray[i]);
-        }
-        return sb.toString(); //return result
-	}
-	
-	// Main method
-	public static void main(String[] args) {
-		String str = "Thi#s i#s a very long stri#ng t####hat I ha#ve #ever seen.#";
-		char re = '#';
-		System.out.println(reChar(str,re));
-	}
+	@BeforeClass
+	  public void beforeClass() {
+		System.out.println("Welcome to use char remover");
+		System.out.println("===========================");
+	  }	
+	@AfterClass
+	  public void afterClass() {
+		System.out.println("===========================");
+		System.out.println("See you~!");
+	  }
+	// Test case1: Positive test.
+		@Test (priority = 1)
+		  public void positiveTest() {
+			String str = "Thi#s i#s a very long stri#ng t####hat I ha#ve #ever seen.#";
+			System.out.println("\nCase1:\nThe original string: "+str);
+			str= str.replace("#","");
+			System.out.println("Remove \"#\" and the result:"+str);
+		  }
+		
+		// Test case2: Negative test.
+		@SuppressWarnings("null")
+		@Test (priority = 2)
+		  public void negativeTest() {
+			String str = null;
+			System.out.println("\nCase2:\nThe original string: "+str);
+			try
+		    {
+				str= str.replace("#","");
+		    }catch (NullPointerException ex)
+			{	
+		    	System.out.println("Bad input:"+ex);
+		    	Assert.fail("NullPointerException was thrown",ex);
+			}
+		  }
+
 }
